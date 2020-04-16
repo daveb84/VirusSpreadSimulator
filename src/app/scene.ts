8c493d008d091f40e0eab48023172e31d41adc7f
David Bywaters <David.Bywaters@parmenion.co.uk>
2020-04-16 09:04:44 +0100

    Add ground image

--- src/app/scene.ts
+++ src/app/scene.ts
@@ -7,8 +7,10 @@ import {
   Color3,
   MeshBuilder,
   StandardMaterial,
+  Texture,
 } from '@babylonjs/core'
-import { minBound, maxBound } from './bounds'
+import { minBound, maxBound, boundsMidpoint, boundsSize } from './bounds'
+import groundImage from '../images/ground.jpg'
 
 export class AppScene {
   private _canvas: HTMLCanvasElement
@@ -34,15 +36,17 @@ export class AppScene {
 
     // Associate a Babylon Engine to it.
     const engine = new Engine(canvas)
+    this._engine = engine
 
     // Create our first scene.
     const scene = new Scene(engine)
+    this._scene = scene
     scene.ambientColor = new Color3(1, 1, 1)
 
     // This creates and positions a free camera (non-mesh)
     const camera = new FreeCamera(
       'camera1',
-      new Vector3(0, maxBound.y * 2, -15),
+      new Vector3(0, maxBound.y * 2, minBound.z * 5),
       scene
     )
     camera.setTarget(Vector3.Zero())
@@ -51,29 +55,42 @@ export class AppScene {
     const light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene)
     light.intensity = 0.7
 
+    this.addBoundingBox()
+    this.addGround()
+  }
+
+  private addBoundingBox() {
     const bounds = MeshBuilder.CreateBox(
       'boundBox',
       {
-        width: maxBound.x - minBound.x,
-        height: maxBound.y - minBound.y,
-        depth: maxBound.z - minBound.z,
+        ...boundsSize,
       },
-      scene
+      this._scene
     )
-    bounds.position.x = 0
-    bounds.position.y = 0
-    bounds.position.z = 0
+    bounds.position = boundsMidpoint
 
-    const material = new StandardMaterial('boundsMaterial', scene)
+    const material = new StandardMaterial('boundsMaterial', this._scene)
     material.diffuseColor = new Color3(1, 1, 1)
     material.wireframe = true
     // material.alpha = 0.3
     // material.backFaceCulling  = false
 
     bounds.material = material
+  }
 
-    this._scene = scene
-    this._engine = engine
+  private addGround() {
+    const ground = MeshBuilder.CreateGround('ground', {
+      width: boundsSize.width,
+      height: boundsSize.depth,
+    })
+    ground.position.x = boundsMidpoint.x
+    ground.position.y = minBound.y
+    ground.position.z = boundsMidpoint.z
+
+    const material = new StandardMaterial('groundMaterial', this._scene)
+    material.diffuseTexture = new Texture(groundImage, this._scene)
+
+    ground.material = material
   }
 
   public start() {
